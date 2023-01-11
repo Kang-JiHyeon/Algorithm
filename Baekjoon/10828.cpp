70e5762c79c6469169ab29a373dab966bca373cf
Kang-JiHyeon <65221707+Kang-JiHyeon@users.noreply.github.com>
2023-01-11 19:59:03 +0900

    Create 10828.cpp
    백준 10828번 스택
    https://www.acmicpc.net/problem/10828

---
+++ Baekjoon/10828.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  stack<int> st;
+  int n;
+  cin >> n;
+  while (n--)
+  {
+    string c;
+    cin >> c;
+    int result;
+
+    if (c == "push")
+    {
+      int k;
+      cin >> k;
+      st.push(k);
+      continue;
+    }
+    else if (c == "pop")
+    {
+      if (st.empty())
+        result = -1;
+      else
+      {
+        result = st.top();
+        st.pop();
+      }
+    }
+    else if (c == "size")
+    {
+      result = st.size();
+    }
+    else if (c == "empty")
+    {
+      result = st.empty() ? 1 : 0;
+    }
+    else if (c == "top")
+    {
+      result = st.empty() ? -1 : st.top();
+    }
+
+    cout << result << '\n';
+  }
+
+  return 0;
+}
